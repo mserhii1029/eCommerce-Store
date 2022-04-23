d7e382ad16085d3c7ef624ff3f5f2fd741c020eb
mserhii1029 <mserhii1029@gmail.com>
2022-04-23 06:19:34 +0300

    Update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './footer.css';
 
@@ -5,24 +6,24 @@ import SocialsMobile from './SocialsFooter';
 import { HiArrowUp } from 'react-icons/hi';
 
 const Footer = () => {
-	return (
-		<footer>
-			<h3 id="contact" className="contact-me">
-				Need a website?
-				<br />
-				Contact me!
-			</h3>
-			<div className="socials-footer">
-				<SocialsMobile />
-			</div>
-			<h5 className="footer-text">© Created by Adrian Delgado 2021</h5>
-			<div className="backup-arrow animation">
-				<a href="#">
-					<HiArrowUp size="3em" color="#2b2b2b" />
-				</a>
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      <h3 id='contact' className='contact-me'>
+        Need a website?
+        <br />
+        Contact me!
+      </h3>
+      <div className='socials-footer'>
+        <SocialsMobile />
+      </div>
+      <h5 className='footer-text'>© Created by Serhii Muzyka 2022</h5>
+      <div className='backup-arrow animation'>
+        <a href='#'>
+          <HiArrowUp size='3em' color='#2b2b2b' />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Footer/SocialsFooter.js
+++ src/components/Footer/SocialsFooter.js
@@ -1,69 +1,28 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import {
-	FiGithub,
-	FiLinkedin,
-	FiInstagram,
-	FiTwitter,
-	FiMail
-} from 'react-icons/fi';
+import { FiGithub, FiLinkedin, FiInstagram, FiTwitter, FiMail } from 'react-icons/fi';
 
 // Creates the social links for the footer
 const Socials = () => {
-	return (
-		<>
-			<a
-				href="https://github.com/adriandelgg"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<FiGithub
-					className="social-footer animation"
-					size="1.5em"
-					color="#2b2b2b"
-				/>
-			</a>
-			<a
-				href="https://www.linkedin.com/in/adriandelgado1/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<FiLinkedin
-					className="social-footer animation"
-					size="1.5em"
-					color="#2b2b2b"
-				/>
-			</a>
-			<a href="mailto:adriandelgado.tech@gmail.com">
-				<FiMail
-					className="social-footer animation"
-					size="1.6em"
-					color="#2b2b2b"
-				/>
-			</a>
-			<a
-				href="https://www.instagram.com/cloudcoding/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<FiInstagram
-					className="social-footer animation"
-					size="1.5em"
-					color="#2b2b2b"
-				/>
-			</a>
-			<a
-				href="https://twitter.com/cloudcoding_"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<FiTwitter
-					className="social-footer animation"
-					size="1.5em"
-					color="#2b2b2b"
-				/>
-			</a>
-		</>
-	);
+  return (
+    <>
+      <a href='https://github.com/mserhii1029' target='_blank' rel='noreferrer'>
+        <FiGithub className='social-footer animation' size='1.5em' color='#2b2b2b' />
+      </a>
+      <a href='#' target='_blank' rel='noreferrer'>
+        <FiLinkedin className='social-footer animation' size='1.5em' color='#2b2b2b' />
+      </a>
+      <a href='mailto:mserhii1029@gmail.com'>
+        <FiMail className='social-footer animation' size='1.6em' color='#2b2b2b' />
+      </a>
+      <a href='#' target='_blank' rel='noreferrer'>
+        <FiInstagram className='social-footer animation' size='1.5em' color='#2b2b2b' />
+      </a>
+      <a href='#' target='_blank' rel='noreferrer'>
+        <FiTwitter className='social-footer animation' size='1.5em' color='#2b2b2b' />
+      </a>
+    </>
+  );
 };
 
 export default Socials;
